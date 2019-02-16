4c878b1e30eb8dd65778347ea93e069e6bbf957b
Gaurav Dabas <dabas0603@gmail.com>
2019-02-16 11:39:03 -0500

    Commented code Temp fix for - "More than one file was found with OS independent path 'META-INF/proguard/androidx-annotations.pro'"

--- app/src/main/java/gauravdabas/com/mlkitocr/MainActivity.java
+++ app/src/main/java/gauravdabas/com/mlkitocr/MainActivity.java
@@ -42,26 +42,34 @@ public class MainActivity extends AppCompatActivity {
     Bitmap capturedImage;
     FirebaseVisionImage visionImage;
 
+    /**
+     * OnCreate will be the first thing android will call during your activity launch
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Getting reference to layout
         imageView = findViewById(R.id.imageView);
         detectedText = findViewById(R.id.detectedText);
         takePhotoButton = findViewById(R.id.cameraButton);
         detectButton = findViewById(R.id.detectButton);
 
+        //Create an output file
         outputFile = new File(
                 this.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
                 "test.jpg");
+
+        //Specify a target URI in which to store the image
         outputUri = FileProvider.getUriForFile(getApplicationContext(),
                 BuildConfig.APPLICATION_ID + ".provider", outputFile);
 
+        //Generate the intent
         final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
 
-
+        //Setup click listener for take photo button
         takePhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,12 +77,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Setup click listener for detect button
         detectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 visionImage = FirebaseVisionImage.fromBitmap(capturedImage);
                 FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
 
+                //Send image to text recognizer for detecting text
                 textRecognizer.processImage(visionImage)
                         .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
                             @Override
@@ -93,14 +103,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /** This method will be called after image capture by Camera */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == TAKE_PICTURE) {
+            //Check if the result includes a thumbnail Bitmap
             if (data != null) {
                 if (data.hasExtra("data")) {
                     Bitmap thumbnail = data.getParcelableExtra("data");
                     imageView.setImageBitmap(thumbnail);
                 } else {
+                    //If there is no thumbnail image data, then the image is stored in target output URI
                     setupBitmapImage();
                     detectButton.setEnabled(true);
                 }
@@ -108,7 +121,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    /** Extract and display text recognized by Firebase Vision
+     *  Detected text is in multiple layer -> Full text, lines or individual text element */
     private void processTextRecognitionResult(FirebaseVisionText text) {
         StringBuilder fullText = new StringBuilder();
         List<FirebaseVisionText.TextBlock> blocks = text.getTextBlocks();
@@ -127,10 +141,13 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        //Set text to display
         detectedText.setText(fullText.toString());
     }
 
+    /** Create Bitmap image from the output file */
     public void setupBitmapImage() {
+        //Resize the full image to fit in out image view
         int width = imageView.getWidth();
         int height = imageView.getHeight();
 
@@ -141,16 +158,19 @@ public class MainActivity extends AppCompatActivity {
         int imageWidth = options.outWidth;
         int imageHeight = options.outHeight;
 
+        //Determine how much to scale down the image
         int scaleFactor = Math.min(imageWidth / width, imageHeight / height);
 
         options.inJustDecodeBounds = false;
         options.inSampleSize = scaleFactor;
 
+        // Decode the image file into a Bitmap sized to fill the View
         Bitmap bitmap = BitmapFactory.decodeFile(outputFile.getPath(), options);
 
         //Rotate imageView for Portrait mode
         Matrix matrix = new Matrix();
         matrix.postRotate(90);
+
         capturedImage = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
 
         imageView.setImageBitmap(capturedImage);
